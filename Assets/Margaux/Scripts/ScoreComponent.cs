dd7ef555c9b55dc59c633133a37e3ed7302cd939
MargauxP93 <prevost.margaux93@gmail.com>
2024-01-27 17:08:30 +0100

    score

--- Assets/Margaux/Scripts/ScoreComponent.cs
+++ Assets/Margaux/Scripts/ScoreComponent.cs
@@ -7,9 +7,6 @@ public class ScoreComponent : MonoBehaviour
 {
     [SerializeField] int score = 0;
     [SerializeField] TextMeshProUGUI scoreTxt = null;
-    [SerializeField] float bonusScore = 50;
-    [SerializeField] float malusScore = -50;
-
     public int Score => score;
     // Start is called before the first frame update
     void Start()
