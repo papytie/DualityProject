063955389f571e8bfa07189691e642b341ccf8f1
MargauxP93 <prevost.margaux93@gmail.com>
2024-01-28 19:58:33 +0100

    end score

--- Assets/Margaux/UI/ScriptsUI/EndScoreUI.cs
+++ Assets/Margaux/UI/ScriptsUI/EndScoreUI.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 
 public class EndScoreUI : MonoBehaviour
 {
+    
     [SerializeField] TextMeshProUGUI endScoreUI = null;
+    [SerializeField] ScoreComponent scoreComponent= null;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +17,10 @@ public class EndScoreUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //UpdateEndScore();
+        UpdateEndScore(scoreComponent.Score);
     }
     public void UpdateEndScore(int _value)
     {
-        endScoreUI.text = $"{_value}";
+        endScoreUI.text = $"Score:\n {_value}";
     }
 }
